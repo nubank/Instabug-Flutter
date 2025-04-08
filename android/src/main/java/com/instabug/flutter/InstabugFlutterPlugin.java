32906705bc462c29e60907d746f884a9b010720e
Bruno Tavares <bruno.tavares@nubank.com.br>
2025-04-08 17:51:08 -0300

    Remove Legacy Android Embedded API

--- android/src/main/java/com/instabug/flutter/InstabugFlutterPlugin.java
+++ android/src/main/java/com/instabug/flutter/InstabugFlutterPlugin.java
@@ -27,22 +27,12 @@ import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.embedding.engine.renderer.FlutterRenderer;
 import io.flutter.plugin.common.BinaryMessenger;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 public class InstabugFlutterPlugin implements FlutterPlugin, ActivityAware {
     private static final String TAG = InstabugFlutterPlugin.class.getName();
 
     @SuppressLint("StaticFieldLeak")
     private static Activity activity;
-
-    /**
-     * Embedding v1
-     */
-    @SuppressWarnings("deprecation")
-    public static void registerWith(Registrar registrar) {
-        activity = registrar.activity();
-        register(registrar.context().getApplicationContext(), registrar.messenger(), (FlutterRenderer) registrar.textures());
-    }
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
